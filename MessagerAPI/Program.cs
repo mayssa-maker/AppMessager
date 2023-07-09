711c658f00dc33e5a812ba0a846bc7f9f8936cf8
mayssa-maker <mattelimayssa@gmail.com>
2023-07-09 20:36:54 +0100

    Db connection/Migration

--- MessagerAPI/Program.cs
+++ MessagerAPI/Program.cs
@@ -1,3 +1,6 @@
+using MessageAppBack.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// Register your DbContext
+builder.Services.AddDbContext<MessagerDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("WebApiDb")));
 
 var app = builder.Build();
 
